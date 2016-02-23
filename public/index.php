c0eeb10d62f5b16cb1460c5e4b374d9e54906dd2
Edwino Stein <edwino.stein@gmail.com>
2016-02-22 21:33:51 -0400

    Adicionando jQuery, Bootstrap e Application ao projeto

--- public/index.php
+++ public/index.php
@@ -3,6 +3,15 @@
     <head>
         <meta charset="utf-8">
         <title>Trabalho 9 de Banco de Dados I</title>
+
+        <!--Styles -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- Scripts -->
+        <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/Application.js"></script>
+        
     </head>
     <body>
         <h1>Trabalho 9 de Banco de Dados I</h1>

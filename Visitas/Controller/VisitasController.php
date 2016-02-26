e8c6761616dac74108fd2695c547a219cfe42194
Edwino Stein <edwino.stein@gmail.com>
2016-02-25 21:49:24 -0400

    Adicionando a action create ao VisitasController

--- Visitas/Controller/VisitasController.php
+++ Visitas/Controller/VisitasController.php
@@ -8,8 +8,11 @@ class VisitasController{
     public function readAction(){
 
         $visitas = LivroVisitas::fetchAll();
-        $data = array();
 
+        if($visitas === false)
+            json_encode(array('success' => false, 'message' => 'Os dados não foram informados corretamente.'));
+
+        $data = array();
         foreach ($visitas as $model)
             $data[] = $model->toArray();
 
@@ -17,7 +20,28 @@ class VisitasController{
     }
 
     public function createAction(){
-        var_dump('create');
+
+        //Verifica se os valores foram informados
+        $erro = false;
+        if(!isset($_POST['nome']) || empty($_POST['nome'])) $erro = true;
+        if(!isset($_POST['localizacao']) || empty($_POST['localizacao'])) $erro = true;
+        if(!isset($_POST['mensagem']) || empty($_POST['localizacao'])) $erro = true;
+
+
+        if($erro){
+            return json_encode(array('success' => false, 'message' => 'Os dados não foram informados corretamente.'));
+        }
+
+        $visita = new LivroVisitas();
+        $visita->setNome($_POST['nome']);
+        $visita->setLocalizacao($_POST['localizacao']);
+        $visita->setMensagem($_POST['mensagem']);
+        $visita->setData(new \DateTime());
+
+        if(!$visita->save())
+            return json_encode(array('success' => false, 'message' => 'Erro na gravção do bano de dados.'));
+
+        return json_encode(array('success' => true, 'data' => $visita->toArray()));
     }
 
 
